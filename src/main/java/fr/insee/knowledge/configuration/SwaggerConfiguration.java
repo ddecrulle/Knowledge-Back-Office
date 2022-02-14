c52fed4bf9a9eadd50af93598dc1c49d80fbaad7
Dylan Decrulle <81740200+ddecrulle@users.noreply.github.com>
2022-02-14 15:07:02 +0100

    Update SwaggerConfiguration.java
    Display on swagger only RestController

--- src/main/java/fr/insee/knowledge/configuration/SwaggerConfiguration.java
+++ src/main/java/fr/insee/knowledge/configuration/SwaggerConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.boot.info.BuildProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.builders.ResponseMessageBuilder;
 import springfox.documentation.service.ApiInfo;
@@ -32,7 +33,7 @@ public class SwaggerConfiguration {
         ArrayList<ResponseMessage> messages = Lists.newArrayList(
                 new ResponseMessageBuilder().code(500).message("Erreur interne du côté serveur").build(),
                 new ResponseMessageBuilder().code(403).message("Interdit!").build());
-        docket.select().apis(RequestHandlerSelectors.basePackage("fr.insee.knowledge.controller")).build()
+        docket.select().apis(RequestHandlerSelectors.withClassAnnotation(RestController.class)).build()
                 .apiInfo(apiInfo()).useDefaultResponseMessages(false).globalResponseMessage(RequestMethod.GET, messages)
                 .securitySchemes(List.of()).securityContexts(List.of());
         return docket;
@@ -42,6 +43,6 @@ public class SwaggerConfiguration {
     private ApiInfo apiInfo() {
         return new ApiInfo(buildProperties.getName(), "Back-office services for Knowledge", buildProperties.getVersion(),
                 "", new Contact("Metallica", "https://github.com/ddecrulle/Knowledge-Back-Office", ""), "LICENSEE",
-                "https://github.com/InseeFr/Queen-Back-Office/blob/master/LICENSE", List.of());
+                "https://github.com/ddecrulle/Knowledge-Back-Office/blob/main/LICENSE", List.of());
     }
 }
