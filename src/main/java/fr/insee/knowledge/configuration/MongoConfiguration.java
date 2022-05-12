42ce497c6c920b77fdb5b42a63bddb73b12ac7aa
Dylan Decrulle <81740200+ddecrulle@users.noreply.github.com>
2022-05-12 14:55:02 +0200

    change mongo uri properties

--- src/main/java/fr/insee/knowledge/configuration/MongoConfiguration.java
+++ src/main/java/fr/insee/knowledge/configuration/MongoConfiguration.java
@@ -3,8 +3,6 @@ package fr.insee.knowledge.configuration;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoDatabase;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,19 +14,10 @@ public class MongoConfiguration {
 
     @Value("${mongodb.database}")
     private String database;
-    @Value("${mongodb.username}")
-    private String username;
-    @Value("${mongodb.password}")
-    private String password;
-    @Value("${mongodb.host}")
-    private String host;
-
-    private final static Logger LOGGER = LoggerFactory.getLogger(MongoConfiguration.class);
-
+    @Value("${mongodb.uri}")
+    private String mongoUri;
 
     private MongoClient mongoClient() {
-        String mongoUri = String.format("mongodb://%s:%s@%s:27017/%s?authSource=%s", username, password, host, database, database);
-        LOGGER.info(String.format("MongoURI : %s", mongoUri));
         return MongoClients.create(mongoUri);
     }
 
