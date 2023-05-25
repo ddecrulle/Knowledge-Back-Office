cdcd7be4bd0e6bff5e3443aeb9a7acbb25af994d
Dylan Decrulle <81740200+ddecrulle@users.noreply.github.com>
2023-05-25 14:28:32 +0200

    Update FunctionServiceImpl.java

--- src/main/java/fr/insee/knowledge/service/impl/FunctionServiceImpl.java
+++ src/main/java/fr/insee/knowledge/service/impl/FunctionServiceImpl.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import fr.insee.knowledge.constants.Constants;
 import fr.insee.knowledge.dao.FunctionDAO;
 import fr.insee.knowledge.domain.Function;
+import fr.insee.knowledge.domain.RawFunction;
 import fr.insee.knowledge.service.FunctionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -43,24 +44,29 @@ public class FunctionServiceImpl implements FunctionService {
 
     private List<Function> getListFunctionsFromJsonNode(JsonNode rootNode) throws JsonProcessingException {
         List<Function> listFunction = new ArrayList<>();
+        List<RawFunction> functionInError = new ArrayList<>();
         if (rootNode.isArray()) {
             for (JsonNode node : rootNode) {
-                recursiveMapping(listFunction, node, new fr.insee.knowledge.domain.Service());
+                recursiveMapping(listFunction, node, new fr.insee.knowledge.domain.Service(), functionInError);
             }
         }
         return listFunction;
     }
 
-    private void recursiveMapping(List<Function> functionList, JsonNode jsonNode, fr.insee.knowledge.domain.Service currentService) throws JsonProcessingException {
+    private void recursiveMapping(List<Function> functionList, JsonNode jsonNode, fr.insee.knowledge.domain.Service currentService, List<RawFunction> functionInError) throws JsonProcessingException {
         currentService.setId(jsonNode.get(Constants.idField).asText());
         currentService.setLabel(jsonNode.get(Constants.labelField).asText());
 
         JsonNode functionNodeArray = jsonNode.get(Constants.functionField);
         if (functionNodeArray != null) {
             for (JsonNode functionNode : functionNodeArray) {
-                Function function = mapper.treeToValue(functionNode, Function.class);
-                function.setService(currentService);
-                functionList.add(function);
+                RawFunction rawFunction = mapper.treeToValue(functionNode, RawFunction.class);
+                rawFunction.setService(currentService);
+
+                // -> get products, user, gsbpm with id in hierarchy db
+                // -> map RawFunction to Function
+
+                functionList.add(rawFunction);
             }
         }
         JsonNode node = jsonNode.get(Constants.serviceField);
