cff2115e78d0d2a194252cb75e534c9e67a675fd
David Darras <david.darras@insee.fr>
2023-05-25 16:44:55 +0200

    feat: import functions

--- src/main/java/fr/insee/knowledge/service/impl/FunctionServiceImpl.java
+++ src/main/java/fr/insee/knowledge/service/impl/FunctionServiceImpl.java
@@ -5,9 +5,18 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import fr.insee.knowledge.constants.Constants;
 import fr.insee.knowledge.dao.FunctionDAO;
+import fr.insee.knowledge.dao.impl.HierarchyGsbpmDAOImpl;
+import fr.insee.knowledge.dao.impl.HierarchyProductDAOImpl;
+import fr.insee.knowledge.dao.impl.HierarchySvcDAOImpl;
+import fr.insee.knowledge.dao.impl.HierarchyUserDAOImpl;
 import fr.insee.knowledge.domain.Function;
+import fr.insee.knowledge.domain.GenericIDLabel;
 import fr.insee.knowledge.domain.RawFunction;
+import fr.insee.knowledge.domain.hierarchy.HierarchyGsbpm;
+import fr.insee.knowledge.domain.hierarchy.HierarchyProduct;
+import fr.insee.knowledge.domain.hierarchy.HierarchyUser;
 import fr.insee.knowledge.service.FunctionService;
+import fr.insee.knowledge.service.exceptions.FunctionValidationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -26,6 +35,19 @@ public class FunctionServiceImpl implements FunctionService {
     private String githubRepository;
     @Autowired
     private FunctionDAO functionDAO;
+
+    @Autowired
+    private HierarchyGsbpmDAOImpl gsbpmDAO;
+
+    @Autowired
+    private HierarchyProductDAOImpl productDAO;
+
+    @Autowired
+    private HierarchyUserDAOImpl userDAO;
+
+    @Autowired
+    private HierarchySvcDAOImpl serviceDAO;
+
     private final ObjectMapper mapper = new ObjectMapper();
 
     public Function getFunctionById(String idValue) {
@@ -43,40 +65,95 @@ public class FunctionServiceImpl implements FunctionService {
     }
 
     private List<Function> getListFunctionsFromJsonNode(JsonNode rootNode) throws JsonProcessingException {
-        List<Function> listFunction = new ArrayList<>();
-        List<RawFunction> functionInError = new ArrayList<>();
-        if (rootNode.isArray()) {
-            for (JsonNode node : rootNode) {
-                recursiveMapping(listFunction, node, new fr.insee.knowledge.domain.Service(), functionInError);
-            }
+        List<Function> functions = new ArrayList<>();
+        List<String> validationErrors = new ArrayList<>();
+        if (!rootNode.isArray()) {
+            return functions;
         }
-        return listFunction;
+
+        for (JsonNode node : rootNode) {
+            fr.insee.knowledge.domain.Service service = new fr.insee.knowledge.domain.Service();
+            service.setId(node.get(Constants.idField).asText());
+            service.setLabel(node.get(Constants.labelField).asText());
+            recursiveMapping(functions, node, service, validationErrors);
+        }
+
+        if (!validationErrors.isEmpty()) {
+            throw new FunctionValidationException("Des erreurs de validation sont survenues", validationErrors);
+        }
+        return functions;
     }
 
-    private void recursiveMapping(List<Function> functionList, JsonNode jsonNode, fr.insee.knowledge.domain.Service currentService, List<RawFunction> functionInError) throws JsonProcessingException {
-        currentService.setId(jsonNode.get(Constants.idField).asText());
-        currentService.setLabel(jsonNode.get(Constants.labelField).asText());
+    private void recursiveMapping(List<Function> functions, JsonNode currentServiceNode, fr.insee.knowledge.domain.Service currentService, List<String> validationErrors) throws JsonProcessingException {
 
-        JsonNode functionNodeArray = jsonNode.get(Constants.functionField);
-        if (functionNodeArray != null) {
-            for (JsonNode functionNode : functionNodeArray) {
-                RawFunction rawFunction = mapper.treeToValue(functionNode, RawFunction.class);
-                rawFunction.setService(currentService);
+        JsonNode functionsNode = currentServiceNode.get(Constants.functionField);
+        addFunctionsFromService(functions, functionsNode, currentService, validationErrors);
 
-                // -> get products, user, gsbpm with id in hierarchy db
-                // -> map RawFunction to Function
-
-                functionList.add(rawFunction);
-            }
+        JsonNode subServicesNode = currentServiceNode.get(Constants.serviceField);
+        if (subServicesNode == null) {
+            return;
         }
-        JsonNode node = jsonNode.get(Constants.serviceField);
-        if (node != null) {
-            for (JsonNode serviceNode : node) {
-                fr.insee.knowledge.domain.Service service = new fr.insee.knowledge.domain.Service();
-                //TODO test if service exist in database
-                service.setService(currentService);
-                recursiveMapping(functionList, serviceNode, service);
+
+        for (JsonNode subServiceNode : subServicesNode) {
+            fr.insee.knowledge.domain.Service subService = new fr.insee.knowledge.domain.Service();
+            subService.setId(subServiceNode.get(Constants.idField).asText());
+            subService.setLabel(subServiceNode.get(Constants.labelField).asText());
+            subService.setService(currentService);
+
+            if (serviceDAO.findById(subService.getId()) == null) {
+                validationErrors.add(String.format("Le service %s n'existe pas", subService.getId()));
             }
+            recursiveMapping(functions, subServiceNode, subService, validationErrors);
+        }
+    }
+
+    private void addFunctionsFromService(List<Function> functions, JsonNode functionsNode,
+                                         fr.insee.knowledge.domain.Service service,
+                                         List<String> validationErrors) throws JsonProcessingException {
+        if (functionsNode == null) {
+            return;
+        }
+
+        for (JsonNode functionNode : functionsNode) {
+            RawFunction rawFunction = mapper.treeToValue(functionNode, RawFunction.class);
+            Function function = new Function();
+            function.setId(rawFunction.getId());
+            function.setIdProduct(rawFunction.getIdProduct());
+            function.setDispo(rawFunction.getDispo());
+            function.setTasks(rawFunction.getTasks());
+            function.setService(service);
+
+            List<GenericIDLabel> products = new ArrayList<>();
+            List<GenericIDLabel> users = new ArrayList<>();
+
+            for (String idProduct : rawFunction.getProducts()) {
+                HierarchyProduct product = productDAO.findById(idProduct);
+                if (product == null) {
+                    validationErrors.add(String.format("Le produit %s n'existe pas pour la fonction %s", idProduct, function.getId()));
+                    continue;
+                }
+                products.add(product);
+            }
+
+            for (String idUser : rawFunction.getUsers()) {
+                HierarchyUser user = userDAO.findById(idUser);
+                if (user == null) {
+                    validationErrors.add(String.format("L'utilisateur %s n'existe pas pour la fonction %s", idUser, function.getId()));
+                    continue;
+                }
+                users.add(user);
+            }
+
+            String idGsbpm = rawFunction.getGsbpm();
+            HierarchyGsbpm gsbpm = gsbpmDAO.findById(idGsbpm);
+            if (gsbpm == null) {
+                validationErrors.add(String.format("Le GSPM %s n'existe pas pour la fonction %s", idGsbpm, function.getId()));
+            }
+
+            function.setGsbpm(gsbpm);
+            function.setProducts(products);
+            function.setUsers(users);
+            functions.add(function);
         }
     }
 }
